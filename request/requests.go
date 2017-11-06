a37e58e2306c96b1c7571dd0ea7b3fa2999cdca1
mostafa-asg <mostafa.asg@gmail.com>
2017-11-06 23:24:16 +0330

    Define wire protocol

---
+++ request/requests.go
@@ -0,0 +1,29 @@
+package request
+
+// Metadata is used by clients to inform the server that the next request will be of `RequestType` type
+type Metadata struct {
+	RequestType string // Request type
+}
+
+// Join is used by users to join to a specific chat room
+type Join struct {
+	Nickname string // Name that be visible to other users on the specific chat room
+	Chatroom string // chatroom name
+}
+
+// Leave is used by users to leave a chatroom
+type Leave struct {
+	Chatroom string //Chatroom name
+}
+
+//Message is used by users to send private message to other users
+type Message struct {
+	RecieverID string
+	Message    string
+}
+
+// Broadcast broadcast a meessage to all users on a specific chatroom
+type Broadcast struct {
+	Chatroom string //Chatroom name
+	Message  string
+}
